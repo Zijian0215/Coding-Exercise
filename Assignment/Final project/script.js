87ae3fd2e985f43b32a22a77578702499465fafd
Zijian0215 <zh2535@columbia.edu>
2023-05-07 01:38:58 -0400

    1

---
+++ Assignment/Final project/script.js
@@ -0,0 +1,44 @@
+const buttons=document.querySelectorAll('.filter-button');
+const images=document.querySelectorAll ('.images img');
+
+buttons.forEach(button=> {
+    button.addEventListener('click',() => {
+        const color = button.dataset.color;
+        images. forEach(image=> {
+            if (image.getAttribute('data-color')===color) {
+                image.style.display = 'block';
+            } else {
+                image.style.display='none'
+            }  
+        });
+    });
+});
+
+
+images. forEach(image=> {
+    image. addEventListener('click',() => {
+        const popupText=image.dataset.popup;
+        const popup =document.createElement('div');
+        popup.classList.add('popup');
+        popup.textContent=popupText;
+        document.body.appendChild(popup);
+    });
+});
+
+image.forEach(image=> {  
+ image. addEventListener('mouseover',event => {
+    const tooltipText=image.dataset.tooltip;
+    const tooltip =document.createElement('div');
+    tooltip.classList.add('tooltip');
+    tooltip.textContent= tooltipText;
+    tooltip.style.top=event.pageY -tooltip.offsetHeight + 'px';
+    tooltip.style.left=event.pageX + 'px';
+    document.body.appendChild(tooltip);
+ });
+    image. addEventListener('mouseout',() => {
+        const tooltip =document.querySelector('.tooltip');
+        if (tooltip) {
+            document.body.remonveChild(tooltip);
+        }  
+    });
+});
